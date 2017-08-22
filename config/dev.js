60a6be833ec21f00064d88afbd5c7cf40fe65537
davisk4rpi <davisk4rpi@gmail.com>
2017-08-22 10:22:10 -0700

    working google oauth flow

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     'mongodb://davis:ha4e7LGSn7nugmdev@ds149743.mlab.com:49743/schedule-dev',
   googleClientID:
     '810296649312-j0kfcnak28iimvord1otapgilauutfp3.apps.googleusercontent.com',
-  googleClientSecret: 'ixIjvuDQCAY0kCyo_D0ybjuv'
+  googleClientSecret: 'ixIjvuDQCAY0kCyo_D0ybjuv',
+  cookieKey: 'jhbiuna9823knfossd8983inosdfon'
 };
