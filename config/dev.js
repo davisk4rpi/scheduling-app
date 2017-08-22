0e0c22bc59ec9103acf1cc602f98fc9884cbcd64
davisk4rpi <davisk4rpi@gmail.com>
2017-08-22 09:37:27 -0700

    add passport services file

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,7 @@
 module.exports = {
   mongoURI:
-    'mongodb://davis:ha4e7LGSn7nugmdev@ds149743.mlab.com:49743/schedule-dev'
+    'mongodb://davis:ha4e7LGSn7nugmdev@ds149743.mlab.com:49743/schedule-dev',
+  googleClientID:
+    '810296649312-j0kfcnak28iimvord1otapgilauutfp3.apps.googleusercontent.com',
+  googleClientSecret: 'ixIjvuDQCAY0kCyo_D0ybjuv'
 };
