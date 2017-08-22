44012f97e00c09f4b69735efaa435e387bdae67e
davisk4rpi <davisk4rpi@gmail.com>
2017-08-22 11:23:42 -0700

    update client routes for heroku

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Landing from './Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Schedulizer</h1>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Route exact path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ client/src/components/Landing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Landing = () => {
+  return <h1>Schedulizer</h1>;
+};
+
+export default Landing;
