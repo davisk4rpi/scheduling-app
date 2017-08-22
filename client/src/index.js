a1f6cebcb8fb89737897c5117d7c302cb456934e
davisk4rpi <davisk4rpi@gmail.com>
2017-08-22 12:04:38 -0700

    remove service worker line

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
 
 import './assets/styles/App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
