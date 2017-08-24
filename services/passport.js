e4148e22c034704c9fb0c41d92032026123b81e2
davisk4rpi <davisk4rpi@gmail.com>
2017-08-23 21:27:15 -0700

    update passport.js to account for multiple authentication strategies

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
@@ -24,14 +25,59 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
+      // find by email to not overwrite someone using multiple strategies
+      const existingUser = await User.findOne({
+        email: profile.emails[0].value
+      });
 
       if (existingUser) {
-        return done(null, existingUser);
+        if (existingUser.googleId === profile.id) {
+          return done(null, existingUser);
+        } else {
+          const updatedUser = await existingUser.update({
+            googleId: profile.id
+          });
+          return done(null, updatedUser);
+        }
       }
 
       const user = await new User({
         googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value
+      }).save();
+      done(null, user);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppID,
+      clientSecret: keys.facebookAppSecret,
+      callbackURL: '/auth/facebook/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      const existingUser = await User.findOne({
+        email: profile.emails[0].value
+      });
+
+      if (existingUser) {
+        if (existingUser.facebookId === profile.id) {
+          return done(null, existingUser);
+        } else {
+          const updatedUser = await existingUser.update({
+            facebookId: profile.id
+          });
+          return done(null, updatedUser);
+        }
+      }
+
+      const user = await new User({
+        facebookId: profile.id,
         name: profile.displayName,
         email: profile.emails[0].value
       }).save();
