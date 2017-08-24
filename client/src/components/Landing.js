9c867d5c37d0bdbef8ba86ebde04f07ae73ad12c
davisk4rpi <davisk4rpi@gmail.com>
2017-08-23 21:55:46 -0700

    add facebook auth proxy to react's package.json

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,6 +5,7 @@ const Landing = () => {
     <div>
       <h1>Schedulizer</h1>
       <a href="/auth/google">Login With Google__</a>
+      <a href="/auth/facebook">Login With Facebook__</a>
       <a href="/api/logout">Logout__</a>
       <a href="/api/current_user">Current User</a>
     </div>
