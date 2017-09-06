4d3e3c36772af3f303fc6520524a1a2846a46c56
davisk4rpi <davisk4rpi@gmail.com>
2017-09-05 17:25:52 -0700

    scaffold out events reducer/action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_EVENTS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   // all import info is attached to the 'data' property of the response
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchEvents = () => async dispatch => {
+  const res = await axios.get('/api/events');
+  dispatch({ type: FETCH_EVENTS, payload: res.data });
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 
 import authReducer from './authReducer';
+import eventsReducer from './eventsReducer';
 
 export default combineReducers({
   auth: authReducer,
+  events: eventsReducer,
   form: reduxForm
 });
