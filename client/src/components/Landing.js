bd84a9f24e963e7bd3fffde63913ac6b2488e6f9
davisk4rpi <davisk4rpi@gmail.com>
2017-09-17 16:38:31 -0700

    add login/signup buttons to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div>
-      <h1>Schedulizer</h1>
+      <h1>Welcome to Schedulizer</h1>
+      <Link to="/login" className="btn-flat grey">
+        Log In
+      </Link>
+      <Link to="/signup" className="btn-flat grey">
+        Sign Up
+      </Link>
     </div>
   );
 };
