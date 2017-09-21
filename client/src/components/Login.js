5b85e35784a805c8c5c5a18a13719925c767668d
davisk4rpi <davisk4rpi@gmail.com>
2017-09-20 17:52:21 -0700

    Edit login/signup page formatting

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,14 +3,21 @@ import React from 'react';
 const Login = props => {
   console.log(props);
   return (
-    <div>
-      <a className="loginBtn loginBtn--facebook" href="/auth/facebook">
-        {props.message} with Facebook
-      </a>
-
-      <a className="loginBtn loginBtn--google" href="/auth/google">
-        {props.message} with Google
-      </a>
+    <div className="center-align" id="loginBox">
+      <h3>
+        Currently to use EZ Flex Planner, you must connect with Facebook or
+        Google
+      </h3>
+      <div>
+        <a className="loginBtn loginBtn--facebook" href="/auth/facebook">
+          {props.message} with Facebook
+        </a>
+      </div>
+      <div>
+        <a className="loginBtn loginBtn--google" href="/auth/google">
+          {props.message} with Google
+        </a>
+      </div>
     </div>
   );
 };
